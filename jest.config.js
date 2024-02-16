846dc382952daff79c239ae9d083db9942986bdb
hari35653565 <137885952+hari35653565@users.noreply.github.com>
2024-02-16 10:47:03 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,19 @@
 module.exports = {
+  globals: {
+    "ts-jest": {
+      skipBabel: true,
+    },
+  },
+  mapCoverage: true,
   moduleFileExtensions: ["js", "ts"],
+  testResultsProcessor: "jest-sonar",
   reporters: ['default',  ['jest-sonar', {
             outputDirectory: '/var/lib/jenkins/workspace/test-jest',
             outputName: 'test-jest.xml',
             reportedFilePath: 'absolute'
         }]],
+  transform: {
+    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+  },
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
 };
